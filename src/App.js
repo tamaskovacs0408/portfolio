8fa434fbd0bc83d0e49f597b6605f28f683dc489
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-08-25 14:49:06 +0200

    Main settings

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Layout/>}/>
+      </Routes>
+    </>
   );
 }
 
