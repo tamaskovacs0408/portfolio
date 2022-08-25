2fb4add2135ad13b9d9ef3cf1be299038baad853
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-08-25 15:58:51 +0200

    Styling navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,17 +1,36 @@
-import { Link } from 'react-router-dom';
-import './index.scss';
-import LogoTK from '../../Assets/Images/tk_pic.png';
-import LogoTKsub from '../../Assets/Images/tamkov.png';
+import { Link, NavLink } from "react-router-dom";
+import "./index.scss";
+import LogoTK from "../../Assets/Images/tk_pic.png";
+import LogoTKsub from "../../Assets/Images/tamkov.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faUser, faEye } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
-    <div className='navbar__container'>
-      <Link className='logo' to ='/' >
+    <div className="navbar__container">
+      <Link className="logo" to="/">
         <img src={LogoTK} alt="tk logo" />
-        <img id='logo_sub'  src={LogoTKsub} alt="tamas kovacs" />
+        <img id="logo_sub" src={LogoTKsub} alt="tamas kovacs" />
       </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#FFFFFF" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#FFFFFF" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className='projects-link' to="/projects">
+          <FontAwesomeIcon icon={faEye} color='#FFFFFF'/>
+        </NavLink>
+      </nav>
+      
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
