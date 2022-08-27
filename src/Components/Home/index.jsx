f5e0364f7d97522a161711203c2c5909e0369cc1
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-08-27 13:42:54 +0200

    Home component styling

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,9 +1,23 @@
-import './index.scss';
+import "./index.scss";
+import LogoTK from "../../Assets/Images/tk_svg.svg";
 
 const Home = () => {
   return (
-    <>Home</>
-  )
-}
+    <div className="home__container">
+      <div className="welcome">
+        <h1>
+          Welcome,
+          <br />
+          my name is <br />
+          <span id="letter_T">T</span>amas!
+        </h1>
+        <span id="title">FRONTEND DEVELOPER</span>
+      </div>
+      <div className="logo_container">
+        <img className="logo_tk" src={LogoTK} alt="logo" />
+      </div>
+    </div>
+  );
+};
 
 export default Home;
--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,11 +1,15 @@
+import { Outlet } from 'react-router-dom';
 import Navbar from '../Navbar';
 import './index.scss';
 
 const Layout = () => {
   return (
-    <>
+    <div className='App'>
       <Navbar />
-    </>
+      <div className="page__container">
+        <Outlet />
+      </div>
+    </div>
   )
 }
 
