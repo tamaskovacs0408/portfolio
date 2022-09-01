3f3181d5da227386abc4a58f5916e060ea999a06
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-01 16:38:43 +0200

    Update style

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -4,8 +4,9 @@ import './index.scss';
 const Projects = () => {
   return (
     <>
-      <div>
-
+      <div className='projects__container'>
+          <h1>MY PROJECTS</h1>
+          <div className='projects'></div>
       </div>
       <Loader type="ball-scale-ripple-multiple"/>
     </>
