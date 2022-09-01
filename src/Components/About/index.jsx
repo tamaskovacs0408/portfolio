eb957350de308d6fd19729cd3175cf96a006eed7
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-01 12:19:00 +0200

    About component main settings

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -1,11 +1,20 @@
 import Loader from "react-loaders";
 import "./index.scss";
+import ProfileImg from '../../Assets/Images/bg_photo.png'
 
 const About = () => {
   return (
     <>
       <div className="about-container">
-        <h1>ABOUT</h1>
+        <div className="text_container">
+          <h1>ABOUT ME</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo amet cumque consectetur accusamus voluptatibus iusto rerum quis illum tenetur odio accusantium corporis quia quisquam impedit error aut molestiae soluta maxime dolore esse, repellat voluptatum minima adipisci. Neque earum repellat reiciendis.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium suscipit unde debitis repudiandae dolorum possimus! Labore voluptate consequatur nostrum iure doloremque, voluptatibus fuga cumque error temporibus ab quaerat eos. Sit ea molestias velit cupiditate totam iure culpa repudiandae eveniet porro!</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo, officiis inventore recusandae sequi facere dolor aut vitae vero provident rerum iure architecto mollitia voluptatibus aliquid reprehenderit veritatis sed tempore voluptas officia itaque, adipisci ab doloribus nobis ad. Laudantium, ducimus officia!</p>
+        </div>
+        <div className="image_container">
+          <img src={ProfileImg} alt="me" />
+        </div>
       </div>
       <Loader type="ball-scale-ripple-multiple" />
     </>
