372c5d2ee8a30805fa1ca978218bd19e8327c7c1
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-07 12:49:32 +0200

    Add project screenshots

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -7,59 +7,75 @@ const Projects = () => {
       <section className="projects__container">
         <h1>MY PROJECTS</h1>
         <article className="inner_container">
-        <div className="projects project_1">
-          <div className="image_container">
-            <img src="" alt="" />
+          <div className="projects project_1">
+            <div className="image_container">
+              <img src="" alt="" />
+            </div>
+            <h2>Project name</h2>
+            <p>Project description</p>
+            <button>
+              <a href="#" rel="noreferrer" target="_blank">
+                Code
+              </a>
+            </button>
+            <button>
+              {" "}
+              <a href="#" rel="noreferrer" target="_blank">
+                Decmo
+              </a>
+            </button>
           </div>
-          <h2>Project name</h2>
-          <p>Project description</p>
-          <button>
-            <a href="">Code</a>
-          </button>
-          <button>
-            {" "}
-            <a href="">Decmo</a>
-          </button>
-        </div>
-        <div className="projects project_2">
-          <div className="image_container">
-            <img src="" alt="" />
+          <div className="projects project_2">
+            <div className="image_container">
+              <img src="" alt="" />
+            </div>
+            <h2>Project name</h2>
+            <p>Project description</p>
+            <button>
+              <a href="#" rel="noreferrer" target="_blank">
+                Code
+              </a>
+            </button>
+            <button>
+              <a href="#" rel="noreferrer" target="_blank">
+                Demo
+              </a>
+            </button>
           </div>
-          <h2>Project name</h2>
-          <p>Project description</p>
-          <button>
-            <a href="">Code</a>
-          </button>
-          <button>
-            <a href="">Demo</a>
-          </button>
-        </div>
-        <div className="projects project_3">
-          <div className="image_container">
-            <img src="" alt="" />
+          <div className="projects project_3">
+            <div className="image_container">
+              <img src="" alt="" />
+            </div>
+            <h2>Project name</h2>
+            <p>Project description</p>
+            <button>
+              <a href="#" rel="noreferrer" target="_blank">
+                Code
+              </a>
+            </button>
+            <button>
+              <a href="#" rel="noreferrer" target="_blank">
+                Demo
+              </a>
+            </button>
           </div>
-          <h2>Project name</h2>
-          <p>Project description</p>
-          <button>
-            <a href="">Code</a>
-          </button>
-          <button>
-            <a href="">Demo</a>
-          </button>
-        </div>
-        <div className="projects project_4">
-          <div className="image_container">
-            <img src="" alt="" />
+          <div className="projects project_4">
+            <div className="image_container">
+              <img src="" alt="" />
+            </div>
+            <h2>Project name</h2>
+            <p>Project description</p>
+            <button>
+              <a href="#" rel="noreferrer" target="_blank">
+                Code
+              </a>
+            </button>
+            <button>
+              <a href="#" rel="noreferrer" target="_blank">
+                Demo
+              </a>
+            </button>
           </div>
-          <h2>Project name</h2>
-          <p>Project description</p>
-          <button>
-            <a href="">Code</a>
-          </button>
-          <button>
-            <a href="">Demo</a>
-          </button>
-        </div>
         </article>
       </section>
       <Loader type="ball-scale-ripple-multiple" />
