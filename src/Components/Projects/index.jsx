30231590a831320be659a0aed93f480f12b34f70
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-07 12:24:53 +0200

    Set up Projects component

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -1,18 +1,70 @@
-import Loader from 'react-loaders';
-import './index.scss';
+import Loader from "react-loaders";
+import "./index.scss";
 
 const Projects = () => {
   return (
     <>
-      <div className='projects__container'>
-          <h1>MY PROJECTS</h1>
-          <div className='projects'>
-            
+      <section className="projects__container">
+        <h1>MY PROJECTS</h1>
+        <article className="inner_container">
+        <div className="projects project_1">
+          <div className="image_container">
+            <img src="" alt="" />
           </div>
-      </div>
-      <Loader type="ball-scale-ripple-multiple"/>
+          <h2>Project name</h2>
+          <p>Project description</p>
+          <button>
+            <a href="">Code</a>
+          </button>
+          <button>
+            {" "}
+            <a href="">Decmo</a>
+          </button>
+        </div>
+        <div className="projects project_2">
+          <div className="image_container">
+            <img src="" alt="" />
+          </div>
+          <h2>Project name</h2>
+          <p>Project description</p>
+          <button>
+            <a href="">Code</a>
+          </button>
+          <button>
+            <a href="">Demo</a>
+          </button>
+        </div>
+        <div className="projects project_3">
+          <div className="image_container">
+            <img src="" alt="" />
+          </div>
+          <h2>Project name</h2>
+          <p>Project description</p>
+          <button>
+            <a href="">Code</a>
+          </button>
+          <button>
+            <a href="">Demo</a>
+          </button>
+        </div>
+        <div className="projects project_4">
+          <div className="image_container">
+            <img src="" alt="" />
+          </div>
+          <h2>Project name</h2>
+          <p>Project description</p>
+          <button>
+            <a href="">Code</a>
+          </button>
+          <button>
+            <a href="">Demo</a>
+          </button>
+        </div>
+        </article>
+      </section>
+      <Loader type="ball-scale-ripple-multiple" />
     </>
-  )
-}
+  );
+};
 
 export default Projects;
