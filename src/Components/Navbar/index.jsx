7322230d55f4fe839f82bea4292dd553a93a0011
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-12 18:10:54 +0200

    Set up mobile vire navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./index.scss";
 import LogoTK from "../../Assets/Images/tk_pic.png";
@@ -8,10 +9,18 @@ import {
   faUser,
   faEye,
   faAt,
+  faBars,
+  faArrowRight,
 } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 const Navbar = () => {
+  const [showNavbar, setshowNavbar] = useState(false);
+
+  const handleClick = () => {
+    setshowNavbar(current => !current)
+  }
+
   return (
     <section className="navbar__container">
       <article className="logo__container">
@@ -68,6 +77,62 @@ const Navbar = () => {
           </li>
         </ul>
       </article>
+      <div className="hamburger_menu_icon">
+        <FontAwesomeIcon onClick={handleClick} icon={faBars} />
+      </div>
+      <div className="mobile_navbar" style={{display: showNavbar ? 'block' : 'none'}}>
+        <nav>
+          <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} />
+          </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="about-link"
+            to="/about"
+          >
+            <FontAwesomeIcon icon={faUser} />
+          </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="projects-link"
+            to="/projects"
+          >
+            <FontAwesomeIcon icon={faEye} />
+          </NavLink>
+        </nav>
+        <article className="contact__container">
+          <ul>
+            <li>
+              <a href="mailto:kovacs.tamas0408@gmail.com" rel="noreferrer">
+                <FontAwesomeIcon icon={faAt} />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/tamaskovacs0408/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faLinkedinIn} color="#FFFFFF" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/tamaskovacs0408"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faGithub} color="#FFFFFF" />
+              </a>
+            </li>
+          </ul>
+          <div className="hamburger_menu_icon">
+            <FontAwesomeIcon onClick={() => setshowNavbar(showNavbar === 'none')} icon={faArrowRight} />
+          </div>
+        </article>
+      </div>
     </section>
   );
 };
