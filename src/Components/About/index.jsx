67e7adf21b5a0102575d4d5b5b3fbecebb3f685a
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-17 11:34:07 +0200

    Remove CV download option

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -1,8 +1,7 @@
 import Loader from "react-loaders";
 import "./index.scss";
 import CV from "../../Assets/CV/tamas_kovacs_cv_en.pdf";
-import ProfilePicture from '../../Assets/Images/profile_photo.png'
-
+import ProfilePicture from "../../Assets/Images/profile_photo.png";
 
 const About = () => {
   return (
@@ -29,11 +28,7 @@ const About = () => {
             >
               LinkedIn
             </a>{" "}
-            profile and you can download my CV on this{" "}
-            <a className="about_link" href={CV} download>
-              link
-            </a>
-            , or you can check out my projects on my{" "}
+            profile, or you can check out my projects on my{" "}
             <a
               className="about_link"
               href="https://github.com/tamaskovacs0408"
