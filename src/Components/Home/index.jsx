0bc7d0b0009d75e13e6fa289da02e01d4e779c6f
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-17 15:11:09 +0200

    Change logo

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,5 +1,5 @@
 import "./index.scss";
-import LogoTK from "../../Assets/Images/Tk_logo-02.png";
+import LogoTK from "../../Assets/Images/tk-logo.png";
 import Loader from "react-loaders";
 
 const Home = () => {
