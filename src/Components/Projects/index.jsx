62552058de22ca1aeb04439405fcaa4849b64572
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-28 21:22:46 +0200

    Add new projects

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -65,6 +65,34 @@ const Projects = () => {
               </a>
             </div>
           </div>
+          <div className="projects project_5">
+            <div className="image_container">
+              <div id="project_5"></div>
+            </div>
+            <h2>Github Authenticator</h2>
+            <div className="link_container">
+              <a className="link_icon" href="https://github.com/tamaskovacs0408/github_auth" rel="noreferrer" target="_blank">
+                <FontAwesomeIcon icon={faCode} />
+              </a>
+              <a className="link_icon" href="https://tamaskovacs0408.github.io/github_auth/" rel="noreferrer" target="_blank">
+                <FontAwesomeIcon icon={faWindowMaximize} />
+              </a>
+            </div>
+          </div>
+          <div className="projects project_6">
+            <div className="image_container">
+              <div id="project_6"></div>
+            </div>
+            <h2>Weather App</h2>
+            <div className="link_container">
+              <a className="link_icon" href="https://github.com/tamaskovacs0408/weather_app" rel="noreferrer" target="_blank">
+                <FontAwesomeIcon icon={faCode} />
+              </a>
+              <a className="link_icon" href="https://tamaskovacs0408.github.io/weather_app/" rel="noreferrer" target="_blank">
+                <FontAwesomeIcon icon={faWindowMaximize} />
+              </a>
+            </div>
+          </div>
         </article>
       </section>
       <Loader type="ball-scale-ripple-multiple" />
