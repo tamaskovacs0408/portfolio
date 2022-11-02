7719690bbe39d49789673f4a64ac088c26a51310
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-11-02 17:18:54 +0100

    Add new projects

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -15,24 +15,44 @@ const Projects = () => {
             </div>
             <h2>Shopping list</h2>
             <div className="link_container">
-              <a className="link_icon" href="https://github.com/tamaskovacs0408/shopping-list" rel="noreferrer" target="_blank">
+              <a
+                className="link_icon"
+                href="https://github.com/tamaskovacs0408/shopping-list"
+                rel="noreferrer"
+                target="_blank"
+              >
                 <FontAwesomeIcon icon={faCode} />
               </a>
-              <a className="link_icon" href="https://tamaskovacs0408.github.io/shopping-list/" rel="noreferrer" target="_blank">
+              <a
+                className="link_icon"
+                href="https://tamaskovacs0408.github.io/shopping-list/"
+                rel="noreferrer"
+                target="_blank"
+              >
                 <FontAwesomeIcon icon={faWindowMaximize} />
               </a>
             </div>
           </div>
           <div className="projects project_2">
             <div className="image_container">
-              <div id='project_2'></div>
+              <div id="project_2"></div>
             </div>
             <h2>Tint and shade generator</h2>
             <div className="link_container">
-              <a className="link_icon" href="https://github.com/tamaskovacs0408/color-generator" rel="noreferrer" target="_blank">
+              <a
+                className="link_icon"
+                href="https://github.com/tamaskovacs0408/color-generator"
+                rel="noreferrer"
+                target="_blank"
+              >
                 <FontAwesomeIcon icon={faCode} />
               </a>
-              <a className="link_icon" href="https://tamaskovacs0408.github.io/color-generator/" rel="noreferrer" target="_blank">
+              <a
+                className="link_icon"
+                href="https://tamaskovacs0408.github.io/color-generator/"
+                rel="noreferrer"
+                target="_blank"
+              >
                 <FontAwesomeIcon icon={faWindowMaximize} />
               </a>
             </div>
@@ -43,10 +63,20 @@ const Projects = () => {
             </div>
             <h2>Hodler Checker</h2>
             <div className="link_container">
-              <a className="link_icon" href="https://github.com/tamaskovacs0408/crypto_app" rel="noreferrer" target="_blank">
+              <a
+                className="link_icon"
+                href="https://github.com/tamaskovacs0408/crypto_app"
+                rel="noreferrer"
+                target="_blank"
+              >
                 <FontAwesomeIcon icon={faCode} />
               </a>
-              <a className="link_icon" href="https://hodler-checker.vercel.app/" rel="noreferrer" target="_blank">
+              <a
+                className="link_icon"
+                href="https://hodler-checker.vercel.app/"
+                rel="noreferrer"
+                target="_blank"
+              >
                 <FontAwesomeIcon icon={faWindowMaximize} />
               </a>
             </div>
@@ -57,10 +87,20 @@ const Projects = () => {
             </div>
             <h2>Recipe finder</h2>
             <div className="link_container">
-              <a className="link_icon" href="https://github.com/tamaskovacs0408/recipes_project" rel="noreferrer" target="_blank">
+              <a
+                className="link_icon"
+                href="https://github.com/tamaskovacs0408/recipes_project"
+                rel="noreferrer"
+                target="_blank"
+              >
                 <FontAwesomeIcon icon={faCode} />
               </a>
-              <a className="link_icon" href="https://tamaskovacs0408.github.io/recipes_project/" rel="noreferrer" target="_blank">
+              <a
+                className="link_icon"
+                href="https://tamaskovacs0408.github.io/recipes_project/"
+                rel="noreferrer"
+                target="_blank"
+              >
                 <FontAwesomeIcon icon={faWindowMaximize} />
               </a>
             </div>
@@ -71,10 +111,20 @@ const Projects = () => {
             </div>
             <h2>Docs</h2>
             <div className="link_container">
-              <a className="link_icon" href="https://github.com/tamaskovacs0408/docs_app" rel="noreferrer" target="_blank">
+              <a
+                className="link_icon"
+                href="https://github.com/tamaskovacs0408/docs_app"
+                rel="noreferrer"
+                target="_blank"
+              >
                 <FontAwesomeIcon icon={faCode} />
               </a>
-              <a className="link_icon" href="https://docs-app-woad.vercel.app/" rel="noreferrer" target="_blank">
+              <a
+                className="link_icon"
+                href="https://docs-app-woad.vercel.app/"
+                rel="noreferrer"
+                target="_blank"
+              >
                 <FontAwesomeIcon icon={faWindowMaximize} />
               </a>
             </div>
@@ -85,11 +135,61 @@ const Projects = () => {
             </div>
             <h2>Weather App</h2>
             <div className="link_container">
-              <a className="link_icon" href="https://github.com/tamaskovacs0408/weather_app" rel="noreferrer" target="_blank">
+              <a
+                className="link_icon"
+                href="https://github.com/tamaskovacs0408/weather_app"
+                rel="noreferrer"
+                target="_blank"
+              >
                 <FontAwesomeIcon icon={faCode} />
               </a>
-              <a className="link_icon" href="https://tamaskovacs0408.github.io/weather_app/" rel="noreferrer" target="_blank">
+              <a
+                className="link_icon"
+                href="https://tamaskovacs0408.github.io/weather_app/"
+                rel="noreferrer"
+                target="_blank"
+              >
                 <FontAwesomeIcon icon={faWindowMaximize} />
+              </a>
+            </div>
+          </div>
+          <div className="projects project_7">
+            <div className="image_container">
+              <div id="project_7"></div>
+            </div>
+            <h2>BMI Calculator</h2>
+            <div className="link_container">
+              <a
+                className="link_icon"
+                href="https://github.com/tamaskovacs0408/bmi_ts"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faCode} />
+              </a>
+              <a
+                className="link_icon"
+                href="https://tamaskovacs0408.github.io/bmi_ts/"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faWindowMaximize} />
+              </a>
+            </div>
+          </div>
+          <div className="projects project_8">
+            <div className="image_container">
+              <div id="project_8"></div>
+            </div>
+            <h2>TKoin Blockchain</h2>
+            <div className="link_container">
+              <a
+                className="link_icon"
+                href="https://github.com/tamaskovacs0408/TKoin"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faCode} />
               </a>
             </div>
           </div>
